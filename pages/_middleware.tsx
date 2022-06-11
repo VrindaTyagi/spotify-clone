c7a133b104cd4d1983ae591ff334c28c87863122
vrindatyagi <vrindatyagi09@gmail.com>
2022-06-11 19:28:22 +0530

    updated package.json

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -5,10 +5,9 @@ const signedinPages = ["/", "/playlist", "/library"];
 export default function middleware(req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
     const token = req.cookies.TRAX_ACCESS_TOKEN;
-    const url = req.nextUrl.clone();
-    url.pathname = "/signin";
+
     if (!token) {
-      return NextResponse.redirect(url);
+      return NextResponse.redirect("/signin");
     }
   }
 }
