87c8af3f82ff84a47ff77a7a03f252da838a9966
vrindatyagi <vrindatyagi09@gmail.com>
2022-08-10 12:47:43 +0530

    changed middleware.js

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,11 +1,10 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const signedinPages = ["/", "/playlist", "/library"];
 
-export default function middleware(req) {
+export default function middleware(req: NextRequest) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
     const token = req.cookies.TRAX_ACCESS_TOKEN;
-
     if (!token) {
       return NextResponse.redirect("/signin");
     }
